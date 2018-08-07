ea9eee52127c655e0c48dc195f996b14c512302d
이인권 <md98765@naver.com>
2018-08-07 14:50:29 +0900

    Calculator sum 함수 추가

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,12 @@ package com.mycompany.test;
 public class Calculator{
 
   public static void main(String[] args){
-
+    itn a = 4;
+    int b = 2;
+    Systemp.out.println(sum(a,b));
   }
 
+  private static int sum(int a,int b){
+    return a+b;
+  }
 }
